c9457e3eab6135f9e16bea53f9c017d8a909bd4c
ASUS\utente <petru.stefano@hotmail.it>
2018-03-22 13:35:32 +0100

    Errors Functions added

--- Aiv.Mpg123.Tests/Mpg123Test.cs
+++ Aiv.Mpg123.Tests/Mpg123Test.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using System.Linq;
-using System.Reflection;
-using System.IO;
 
 namespace Aiv.Mpg123.Tests
 {
@@ -21,6 +19,20 @@ namespace Aiv.Mpg123.Tests
         {
 
             Assert.That(Mpg123.PlainStrError(Mpg123.Errors.OK), Is.Not.Null);
+        }
+
+        [Test]
+        public void TestStrError()
+        {
+            Mpg123 mpg123 = new Mpg123();
+            Assert.That(Mpg123.StrError(mpg123.handle), Is.Not.Null);
+        }
+
+        [Test]
+        public void TestErrorCode()
+        {
+            Mpg123 mpg123 = new Mpg123();
+            Assert.That(Mpg123.ErrorCode(mpg123.handle), Is.Not.Null);
         }
 
         [Test]
@@ -57,47 +69,5 @@ namespace Aiv.Mpg123.Tests
             Assert.That(mpg123.HasValidHandle, Is.True);
         }
 
-        [Test]
-        public void TestOpenPathNull()
-        {
-            Mpg123 mpg123 = new Mpg123();
-            Assert.That(() => mpg123.Open(null), Throws.Nothing);
-        }
-
-        [Test]
-
-        public void TestOpenPathInvalid()
-        {
-            string path = "test.mp3";
-            Mpg123 mpg123 = new Mpg123();
-            Assert.That(() => mpg123.Open(path), Throws.TypeOf<Mpg123.ErrorException>());
-        }
-
-        [Test]
-        public void TestOpenPathValid()
-        {
-            string dirName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string path = dirName + "/bensound-epic.mp3";
-            Mpg123 mpg123 = new Mpg123();
-            Assert.That(() => mpg123.Open(path), Throws.Nothing);
-        }
-
-        [Test]
-        public void TestCloseNoOpen()
-        {
-            Mpg123 mpg123 = new Mpg123();
-            Assert.That(() => mpg123.Close(), Throws.Nothing);
-        }
-
-        [Test]
-        public void TestCloseOpen()
-        {
-            string dirName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string path = dirName + "/bensound-epic.mp3";
-
-            Mpg123 mpg123 = new Mpg123();
-            mpg123.Open(path);
-            Assert.That(() => mpg123.Close(), Throws.Nothing);
-        }
     }
 }
--- Aiv.Mpg123/NativeMethods.cs
+++ Aiv.Mpg123/NativeMethods.cs
@@ -9,11 +9,18 @@ namespace Aiv.Mpg123
         internal const string LibraryName = "libmpg123-0.dll";
 
         [DllImport(LibraryName, EntryPoint = "mpg123_init", CallingConvention = CallingConvention.Cdecl)]
+        // aspetta che ti passo un valore 32bit e castamelo ad Mpg123.Errors
         [return: MarshalAs(UnmanagedType.I4)]
         internal extern static Mpg123.Errors NativeMpg123Init();
 
         [DllImport(LibraryName, EntryPoint = "mpg123_plain_strerror", CallingConvention = CallingConvention.Cdecl)]
         internal extern static IntPtr NativeMpg123PlainStrError([MarshalAs(UnmanagedType.I4)] Mpg123.Errors error);
+
+        [DllImport(LibraryName, EntryPoint = "mpg123_strerror", CallingConvention = CallingConvention.Cdecl)]
+        internal extern static IntPtr NativeMpg123StrError(IntPtr handle);
+
+        [DllImport(LibraryName, EntryPoint = "mpg123_errcode", CallingConvention = CallingConvention.Cdecl)]
+        internal extern static int NativeMpg123ErrorCode(IntPtr handle);
 
         [DllImport(LibraryName, EntryPoint = "mpg123_decoders", CallingConvention = CallingConvention.Cdecl)]
         internal extern static IntPtr NativeMpg123Decoders();
@@ -23,19 +30,6 @@ namespace Aiv.Mpg123
 
         [DllImport(LibraryName, EntryPoint = "mpg123_delete", CallingConvention = CallingConvention.Cdecl)]
         internal extern static void NativeMpg123Delete(IntPtr handle);
-
-        [DllImport(LibraryName, EntryPoint = "mpg123_open", CallingConvention = CallingConvention.Cdecl)]
-        [return: MarshalAs(UnmanagedType.I4)]
-        internal extern static Mpg123.Errors NativeMpg123Open(IntPtr handle, IntPtr path);
-
-        [DllImport(LibraryName, EntryPoint = "mpg123_close", CallingConvention = CallingConvention.Cdecl)]
-        [return: MarshalAs(UnmanagedType.I4)]
-        internal extern static Mpg123.Errors NativeMpg123Close(IntPtr handle);
-
-        [DllImport(LibraryName, EntryPoint = "mpg123_read", CallingConvention = CallingConvention.Cdecl)]
-        [return: MarshalAs(UnmanagedType.I4)]
-        internal extern static Mpg123.Errors NativeMpg123Read(IntPtr handle, IntPtr outMemory, UIntPtr outMemSize, UIntPtr done);
-
 
 
     }
